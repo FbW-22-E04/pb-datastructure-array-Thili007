7f2e123d3a11a17defa02be399a988785749a4a0
Your Name <tpunchihewage@gmail.com>
2022-07-07 14:51:58 +0200

    modified solutions.js file

---
+++ solution.js
@@ -0,0 +1,11 @@
+const myArray = ["mango", "banana", "apple", "kiwi", "woodapple"];
+
+console.log(myArray);
+
+let thirdElement = myArray[2];
+
+console.log(thirdElement);
+
+let fourthElement = myArray[3];
+
+console.log(fourthElement);
